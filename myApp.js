f80fe9a5381e37f5336d73062296f108eda0cebb
HIEU Le Trung <hieuletrung102@gmail.com>
2018-12-05 19:04:55 +0700

    Finish tak8 chaining middleware

--- myApp.js
+++ myApp.js
@@ -48,6 +48,27 @@ app.get('/json', (req, res) => {
 
 /** 8) Chaining middleware. A Time server */
 
+// This approach is useful to split the server operations into smaller units. 
+// That leads to a better app structure, and the possibility to reuse code in different places. 
+// This approach can also be used to perform some validation on the data. 
+// At each point of the middleware stack you can block the execution of the current chain 
+// and pass control to functions specifically designed to handle errors. 
+// Or you can pass control to the next matching route, to handle special cases. 
+// We will see how in the advanced Express section.
+
+// In the route app.get('/now', ...) chain a middleware function and the final handler. 
+// In the middleware function you should add the current time to the request object in the req.time key. 
+// You can use new Date().toString(). In the handler, respond with a JSON object, 
+// taking the structure {time: req.time}.
+
+app.get('/now',
+    (req, res, next) => {
+        req['time'] = new Date().toString()
+        next()
+    },
+    (req, res) => {
+        res.json({time: req.time})
+    })
 
 /** 9)  Get input from client - Route parameters */
 
